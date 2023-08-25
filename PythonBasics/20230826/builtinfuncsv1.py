e0a621423907db47ce196306c5cd363b056a6ea8
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-08-25 13:10:00 +0530

    Create builtinfuncsv1.py

---
+++ PythonBasics/20230826/builtinfuncsv1.py
@@ -0,0 +1,16 @@
+# Demo of few of the Built in functions in Python
+
+value = -45
+print("dir(value): ", dir(value))
+
+name = "Python"
+print("dir(name): ", dir(name))
+
+print(print)
+print("dir(print): ", dir(print))
+
+print(abs)
+print("dir(abs): ", dir(abs))
+
+print(dir)
+print(dir(__builtins__))
