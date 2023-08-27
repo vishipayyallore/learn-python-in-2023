55a036118e15c44bb1ae9c63c73f8c3e5a9e65e3
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-08-27 07:48:43 +0530

    Swamy/25aug work (#4)
    * Changes
    
    * Changes
    
    * Update ReadMe.md
    
    * Create builtinfuncs.py
    
    * Update builtinfuncs.py
    
    * Create builtinfuncsv1.py
    
    * Update maindatatypesdemo.py
    
    * Update ReadMe.md
    
    * Update datatypesdemo.py
    
    * Update datatypesdemo.py

--- PythonBasics/20230825/datatypesdemo.py
+++ PythonBasics/20230825/datatypesdemo.py
@@ -7,47 +7,62 @@
 # Binary Types:	bytes, bytearray, memoryview
 # None Type:	NoneType
 
+# Integer
 age = 20
 print("Age: ", age, "Type: ", type(age))
 
+# String
 name = "John"
 print("Name: ", name, "Type: ", type(name))
 
+# Float
 salary = 1000.0
 print("Salary: ", salary, "Type: ", type(salary))
 
+# Complex
 complex_number = 1 + 2j
 print("Complex Number: ", complex_number, "Type: ", type(complex_number))
 
+# List
 hobbies = ["Reading", "Writing", "Coding"]
-print("Hobbies: ", hobbies, "Type: ", type(hobbies))
+print("List of Hobbies: ", hobbies, "Type: ", type(hobbies))
 
+# Tuple
 hobbies = ("Reading", "Writing", "Coding")
-print("Hobbies: ", hobbies, "Type: ", type(hobbies))
+print("Tuple Hobbies: ", hobbies, "Type: ", type(hobbies))
 
+# Range
 range_numbers = range(10)
 print("Range Numbers: ", range_numbers, "Type: ", type(range_numbers))
 
+# Dictionary
 dictionary = {"name": "John", "age": 20}
 print("Dictionary: ", dictionary, "Type: ", type(dictionary))
 
+# Set
 set_numbers = {1, 2, 3, 4, 5}
 print("Set Numbers: ", set_numbers, "Type: ", type(set_numbers))
 
+# Frozen Set
 hobbies = frozenset({"Reading", "Writing", "Coding"})
-print("Hobbies: ", hobbies, "Type: ", type(hobbies))
+print("Frozen Set Hobbies: ", hobbies, "Type: ", type(hobbies))
 
+# Boolean
 is_active = True
-print("Is Active: ", is_active, "Type: ", type(is_active))
+print("Bool Is Active: ", is_active, "Type: ", type(is_active))
 
+# Binary Data
 binary_data = b"Hello"
 print("Binary Data: ", binary_data, "Type: ", type(binary_data))
 
+# Byte Array
 binary_array = bytearray(5)
 print("Binary Array: ", binary_array, "Type: ", type(binary_array))
 
+# Memory View
 memory_view = memoryview(bytes(5))
 print("Memory View: ", memory_view, "Type: ", type(memory_view))
 
+# None Type
 none_type = None
 print("None Type: ", none_type, "Type: ", type(none_type))
--- PythonBasics/ReadMe.md
+++ PythonBasics/ReadMe.md
@@ -9,3 +9,19 @@
 > 1. Python is a case sensitive language.
 > 1. All Entities in Python are objects. String, List, Tuple, Dictionary, Set, etc. are all objects. Even Functions are objects.
 > 1. Object is an instance of a class. Class is a blueprint of an object.
+
+## Functions
+
+```python
+# Snake case is used for function names
+def my_function(param1, param2):
+  print("Hello from a function")
+  return param1 + param2
+
+# Calling a function
+my_function(1, 2)
+```
+
+```
+
+```
