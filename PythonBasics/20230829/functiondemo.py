1d2f8c127be96b916644d03a0cef51ef9d8b1f78
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-08-29 11:25:56 +0530

    Update functiondemo.py

--- PythonBasics/20230829/functiondemo.py
+++ PythonBasics/20230829/functiondemo.py
@@ -15,12 +15,18 @@ def divide_numbers(a, b):
     return a / b
 
 
+def say_hello():  # This function does not return a value. None is returned by default.
+    print("Hello, world!")
+
+
 def main():
     print("This is a function demo.")
     print("The result of 4 * 5 is", multiply_numbers(4, 5))
     print("The result of 4 + 5 is", add_numbers(4, 5))
     print("The result of 4 - 5 is", subtract_numbers(4, 5))
     print("The result of 4 / 5 is", divide_numbers(4, 5))
+    # None is returned by default.
+    print("The result of say_hello() is", say_hello())
 
 
 main()
