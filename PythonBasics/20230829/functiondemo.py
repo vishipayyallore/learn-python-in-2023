d05b9a4a4c96434c1480dd770427fc7a6da138eb
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-08-29 11:20:48 +0530

    Create functiondemo.py

---
+++ PythonBasics/20230829/functiondemo.py
@@ -0,0 +1,26 @@
+
+def multiply_numbers(a, b):
+    return a * b
+
+
+def add_numbers(a, b):
+    return a + b
+
+
+def subtract_numbers(a, b):
+    return a - b
+
+
+def divide_numbers(a, b):
+    return a / b
+
+
+def main():
+    print("This is a function demo.")
+    print("The result of 4 * 5 is", multiply_numbers(4, 5))
+    print("The result of 4 + 5 is", add_numbers(4, 5))
+    print("The result of 4 - 5 is", subtract_numbers(4, 5))
+    print("The result of 4 / 5 is", divide_numbers(4, 5))
+
+
+main()
