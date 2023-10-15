f7d08bef6bfafd5bebb115613e98349d202db22c
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-10-15 21:09:48 +0530

    Working Version

--- flask-azoai/static/index.js
+++ flask-azoai/static/index.js
@@ -1,11 +1,17 @@
 function generateResponse() {
     const userInput = document.getElementById("userInput").value;
     const responseContainer = document.getElementById("response");
+    const chatList = document.getElementById("chat-list");
+
+    // Append the user's input to the chat
+    chatList.innerHTML += `<li class="list-group-item"><b>You:</b> ${userInput}</li>`;
 
     // Make an API request to your Flask API
     fetch("/api/get?msg=" + userInput)
         .then(response => response.text())
         .then(data => {
+            // Append the AI's response to the chat
+            chatList.innerHTML += `<li class="list-group-item"><b>OpenAI:</b> ${data}</li>`;
             responseContainer.innerText = data;
         })
         .catch(error => {
