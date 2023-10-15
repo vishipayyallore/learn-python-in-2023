1d59fed028acacd42f26fbcd28666ffd6a2942c2
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-10-15 20:16:27 +0530

    Update app.py

--- flask-azoai/app.py
+++ flask-azoai/app.py
@@ -1,10 +1,14 @@
 import os
-from flask import Flask, request, render_template
 import openai
+from flask import Flask, request, render_template
+
+# Load config values
+from dotenv import dotenv_values
+config_details = dotenv_values(".env")
 
 openai.api_type = "azure"
-openai.api_base = "https://azure-YOUR-dev-001.openai.azure.com/"
-openai.api_version = "2022-12-01"
+openai.api_base = config_details['OPENAI_API_BASE']
+openai.api_version = config_details['OPENAI_API_VERSION']
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
 app = Flask(__name__, static_url_path='/static')
@@ -15,21 +19,30 @@ def index():
     return render_template("index.html")
 
 
-@app.route("/get")
-def get_bot_response():
+@app.route("/api/get")
+def get_completion_response():
     userText = request.args.get('msg')
-    response = openai.Completion.create(
-        engine="gpt-35-turbo-dev-001",
-        prompt=userText,
-        temperature=1,
-        max_tokens=150,
-        top_p=0.5,
-        frequency_penalty=0,
-        presence_penalty=0,
-        stop=None
-    )
-    answer = response.choices[0].text
-    return str(answer)
+    print("User Text: ", userText)
+    user_prompt = "Tell me a joke on software engineering.\n\n"
+
+    print('Azure Open AI Key: ', openai.api_key)
+
+    try:
+        response = openai.Completion.create(
+            engine=config_details['COMPLETIONS_MODEL'],
+            prompt=user_prompt,
+            temperature=1,
+            max_tokens=150,
+            top_p=0.5,
+            frequency_penalty=0,
+            presence_penalty=0,
+            stop=None)
+
+        answer = response.choices[0].text
+        return str(answer)
+    except:
+        print("An exception has occurred. \n")
+        # print("Error Message:", response)
 
 
 if __name__ == "__main__":
