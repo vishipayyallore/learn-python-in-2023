4aa49bfdb9213f41c4aa1bd723ea0aacb1f40626
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-10-16 12:17:33 +0530

    Swamy/11oct stores flaskapi day1 (#16)
    * Create ReadMe.md
    
    * Create .gitignore
    
    * Initial Version
    
    * Changes
    
    * Verified the execution inside Docker
    
    * Update ReadMe.md
    
    * Create ReadMe.md
    
    * Initial Version
    
    * Changes
    
    * Initial Version
    
    * Changes
    
    * Axios Installed
    
    * Changes
    
    * Initial Version
    
    * .gitignore file
    
    * Changes
    
    * Create requirements.txt
    
    * Update app.py
    
    * Update app.py
    
    * Working Version
    
    * Changes
    
    * Changes
    
    * Working Version
    
    * Changes

--- flask-azoai/static/index.js
+++ flask-azoai/static/index.js
@@ -4,14 +4,15 @@ function generateResponse() {
     const chatList = document.getElementById("chat-list");
 
     // Append the user's input to the chat
-    chatList.innerHTML += `<li class="list-group-item"><b>You:</b> ${userInput}</li>`;
+    chatList.innerHTML += `<li class="list-group-useritem"><b>You:</b> ${userInput}</li>`;
 
     // Make an API request to your Flask API
     fetch("/api/get?msg=" + userInput)
         .then(response => response.text())
         .then(data => {
             // Append the AI's response to the chat
-            chatList.innerHTML += `<li class="list-group-item"><b>OpenAI:</b> ${data}</li>`;
+            chatList.innerHTML += `<li class="list-group-azoaiitem"><b>OpenAI:</b> ${data}</li>`;
+            console.log("Success:", data);
             responseContainer.innerText = data;
         })
         .catch(error => {
