19a9a994fac6f74ed4c70bc206792a7ed0ee4c28
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-10-29 12:17:35 +0530

    Swamy/29oct work (#21)
    * Update functional_tests.py
    
    * home.html
    
    * Code Refactoring
    
    * Update tests.py
    
    * Chapter 4

--- tddpygoatbook/lists/tests.py
+++ tddpygoatbook/lists/tests.py
@@ -6,23 +6,34 @@ from lists.views import home_page
 
 
 class HomePageTest(TestCase):
-    def test_home_page_returns_correct_html(self):
-        request = HttpRequest()
-        response = home_page(request)
-        html = response.content.decode("utf8")
-        self.assertIn("<title>To-Do lists</title>", html)
-        self.assertTrue(html.startswith("<html>"))
-        self.assertTrue(html.endswith("</html>"))
-
-    def test_home_page_returns_correct_html_2(self):
+    def test_uses_home_template(self):
         response = self.client.get("/")
-        self.assertContains(response, "<title>To-Do lists</title>")
+        self.assertTemplateUsed(response, "home.html")
 
-    def test_home_page_returns_correct_html_original(self):
+    def test_home_page_returns_correct_html(self):
         response = self.client.get("/")
         self.assertContains(response, "<title>To-Do lists</title>")
         self.assertContains(response, "<html>")
         self.assertContains(response, "</html>")
+        self.assertTemplateUsed(response, "home.html")
+
+    # def test_home_page_returns_correct_html_2(self):
+    #     response = self.client.get("/")
+    #     self.assertContains(response, "<title>To-Do lists</title>")
+
+    # def test_home_page_returns_correct_html_original(self):
+    #     response = self.client.get("/")
+    #     self.assertContains(response, "<title>To-Do lists</title>")
+    #     self.assertContains(response, "<html>")
+    #     self.assertContains(response, "</html>")
+
+    # def test_home_page_returns_correct_html(self):
+    #     request = HttpRequest()
+    #     response = home_page(request)
+    #     html = response.content.decode("utf8")
+    #     self.assertIn("<title>To-Do lists</title>", html)
+    #     self.assertTrue(html.startswith("<html>"))
+    #     self.assertTrue(html.endswith("</html>"))
 
 
 class SmokeTest(TestCase):
