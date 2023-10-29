95587b003c8c0630f1fc4919e355531e757bc897
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-10-29 10:19:45 +0530

    Swamy/28oct work (#20)
    * Initial version
    
    * Unit Tests
    
    * First TDD
    
    * Unit Test Works!
    
    * Update ReadMe.md

--- tddpygoatbook/ReadMe.md
+++ tddpygoatbook/ReadMe.md
@@ -3,6 +3,12 @@
 ## Few Commands
 
 > 1. We are using git bash for windows
+> 1. Running the Django dev server
+>    - python manage.py runserver
+> 1. Running the functional tests
+>    - python functional_tests.py
+> 1. Running the unit tests
+>    - python manage.py test
 
 ```bash
 mkdir tddpygoatbook
@@ -23,11 +29,15 @@ django-admin startproject superlists .
 
 git status
 git diff
+git diff -w
 
 git add .
+git commit -a # The -a means “automatically add any changes to tracked files
 git commit -m "First commit"
 
 python manage.py startapp lists
 
 python manage.py test
+
+python manage.py runserver
 ```
--- tddpygoatbook/lists/tests.py
+++ tddpygoatbook/lists/tests.py
@@ -1,8 +1,30 @@
 from django.test import TestCase
+from django.http import HttpRequest
+from lists.views import home_page
 
 # Create your tests here.
 
 
+class HomePageTest(TestCase):
+    def test_home_page_returns_correct_html(self):
+        request = HttpRequest()
+        response = home_page(request)
+        html = response.content.decode("utf8")
+        self.assertIn("<title>To-Do lists</title>", html)
+        self.assertTrue(html.startswith("<html>"))
+        self.assertTrue(html.endswith("</html>"))
+
+    def test_home_page_returns_correct_html_2(self):
+        response = self.client.get("/")
+        self.assertContains(response, "<title>To-Do lists</title>")
+
+    def test_home_page_returns_correct_html_original(self):
+        response = self.client.get("/")
+        self.assertContains(response, "<title>To-Do lists</title>")
+        self.assertContains(response, "<html>")
+        self.assertContains(response, "</html>")
+
+
 class SmokeTest(TestCase):
     def test_bad_maths(self):
-        self.assertEqual(1 + 1, 3)
+        self.assertEqual(1 + 2, 3)
