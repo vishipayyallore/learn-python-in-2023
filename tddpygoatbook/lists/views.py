06f15eb92c9528b069b17b4664d129c9ca05d9c8
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-10-30 12:06:55 +0530

    Changes

--- tddpygoatbook/lists/views.py
+++ tddpygoatbook/lists/views.py
@@ -1,10 +1,14 @@
-from django.http import HttpResponse
 from django.shortcuts import render
+from lists.models import Item
 
 # Create your views here.
 
 
 def home_page(request):
+    item = Item()
+    item.text = request.POST.get("item_text", "")
+    item.save()
+
     return render(
         request,
         "home.html",
