f8383f8a8ab713756fcd840102d1b5457ede95d4
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-10-30 12:07:11 +0530

    Update functional_tests.py

--- tddpygoatbook/functional_tests.py
+++ tddpygoatbook/functional_tests.py
@@ -32,10 +32,15 @@ class NewVisitorTest(unittest.TestCase):
         self.assertEqual(inputbox.get_attribute(
             "placeholder"), "Enter a to-do item")
 
+        # She types "Buy peacock feathers" into a text box
+        # (Edith's hobby is tying fly-fishing lures)
+        inputbox.send_keys("Buy peacock feathers")
+
         # When she hits enter, the page updates, and now the page lists
         # "1: Buy peacock feathers" as an item in a to-do list table
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
+        self.check_for_row_in_list_table("1: Buy peacock feathers")
 
         # There is still a text box inviting her to add another item.
         # She enters "Use peacock feathers to make a fly"
@@ -54,31 +59,4 @@ class NewVisitorTest(unittest.TestCase):
 
 
 if __name__ == "__main__":
-    unittest.main(warnings="ignore")
-
-# browser = webdriver.Firefox()
-# browser.get('http://localhost:8000')
-
-# Edith has heard about a cool new online to-do app.
-# She goes to check out its homepage
-# browser.get("http://localhost:8000")
-
-# She notices the page title and header mention to-do lists
-# assert "To-Do" in browser.title
-# print(browser.title)
-# print('OK')
-
-# She is invited to enter a to-do item straight away
-
-# She types "Buy peacock feathers" into a text box
-# (Edith's hobby is tying fly-fishing lures)
-
-# When she hits enter, the page updates, and now the page lists
-# "1: Buy peacock feathers" as an item in a to-do list
-
-# There is still a text box inviting her to add another item.
-# She enters "Use peacock feathers to make a fly" (Edith is very methodical)
-
-# The page updates again, and now shows both items on her list
-
-# Satisfied, she goes back to sleep
+    unittest.main()
