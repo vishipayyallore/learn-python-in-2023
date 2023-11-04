975504f9ad51b1c1c82b5047b8668cae11617881
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-11-04 15:16:57 +0530

    Changes

--- tddpygoatbook/lists/tests.py
+++ tddpygoatbook/lists/tests.py
@@ -15,8 +15,7 @@ class HomePageTest(TestCase):
 
     def test_redirects_after_POST(self):
         response = self.client.post("/", data={"item_text": "A new list item"})
-        self.assertRedirects(response, "/lists/the-only-list-in-the-world/")
-        # self.assertRedirects(response, "/")
+        self.assertRedirects(response, "/")
 
     def test_only_saves_items_when_necessary(self):
         self.client.get("/")
@@ -24,6 +23,10 @@ class HomePageTest(TestCase):
 
 
 class ListViewTest(TestCase):
+    def test_uses_list_template(self):
+        response = self.client.get("/lists/the-only-list-in-the-world/")
+        self.assertTemplateUsed(response, "list.html")
+
     def test_displays_all_list_items(self):
         Item.objects.create(text="itemey 1")
         Item.objects.create(text="itemey 2")
