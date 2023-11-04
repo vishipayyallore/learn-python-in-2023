91c35a5d8ae169ee48825fa0ebe0b33148dd350e
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-11-04 14:54:28 +0530

    Update tests.py

--- tddpygoatbook/functional_tests/tests.py
+++ tddpygoatbook/functional_tests/tests.py
@@ -52,4 +52,33 @@ class NewVisitorTest(LiveServerTestCase):
         self.wait_for_row_in_list_table(
             "2: Use peacock feathers to make a fly")
 
-        # Satisfied, she goes back to sleep
+    def test_multiple_users_can_start_lists_at_different_urls(self):
+        self.browser.get(self.live_server_url)
+        inputbox = self.browser.find_element(By.ID, "id_new_item")
+        inputbox.send_keys("Buy peacock feathers")
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table("1: Buy peacock feathers")
+
+        edith_list_url = self.browser.current_url
+        self.assertRegex(edith_list_url, "/lists/.+")
+
+        self.browser.delete_all_cookies()
+
+        # 2nd user visits the home page.  There is no sign of Edith's
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element(By.TAG_NAME, "body").text
+        self.assertNotIn("Buy peacock feathers", page_text)
+        self.assertNotIn("make a fly", page_text)
+
+        inputbox = self.browser.find_element(By.ID, "id_new_item")
+        inputbox.send_keys("Buy milk")
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table("1: Buy milk")
+
+        francis_list_url = self.browser.current_url
+        self.assertRegex(francis_list_url, "/lists/.+")
+        self.assertNotEqual(francis_list_url, edith_list_url)
+
+        page_text = self.browser.find_element(By.TAG_NAME, "body").text
+        self.assertNotIn("Buy peacock feathers", page_text)
+        self.assertIn("Buy milk", page_text)
